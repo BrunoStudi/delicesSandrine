5ecf29f3aa4aec6cd8f1193c2bd0581b10002049
BrunoStudi <r.brunocarriere@protonmail.com>
2024-02-15 21:27:23 +0100

    systeme de commentaires fonctionnel, bouton collapse en cours

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -13,27 +13,34 @@ use App\Entity\Article;
 class CommentController extends AbstractController
 {
     //#[Route('/recette/{articleId}/commentaire/ajout', name: 'commentaire_ajout', methods: 'POST')]
-    public function ajout(PersistenceManagerRegistry $doctrine, Request $request, Article $article): Response
+    public function ajout(PersistenceManagerRegistry $doctrine, Request $request): Response
     {
+        $articleId = $request->request->get('article_id');
         $contenu = $request->request->get('contenu');
+        $em = $doctrine->getManager();
+
+        // Récupérer l'article correspondant à partir de son ID
+        $article = $em->getRepository(Article::class)->find($articleId);
+
+        //Vérifier si l'article existe en BDD.
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé pour l\'ID ' . $articleId);
+        }
 
         $comments = new Comment();
         $comments->setContent($contenu);
         $comments->setArticle($article);
 
-        $em = $doctrine->getManager();
-        $this->saveComment($doctrine, $comments, $article);
+        $this->saveComment($doctrine, $comments);
 
         return $this->redirectToRoute('article_show', ['id' => $article->getId()]);
     }
 
     private function completeCommentBeforeSave(Comment $comments) {
         if($comments->getPublishedAt()){
-            $comments->getPublishedAt(new \DateTime());
             $comments->setPublishedAt(new \DateTime());
         }
         $comments->setAuthor($this->getUser());
-        $comments->setPublishedAt(new \DateTime());
     
         return $comments;
     }
@@ -43,12 +50,11 @@ class CommentController extends AbstractController
      * 
      * @param   Comment     $comments
      */
-    private function saveComment(PersistenceManagerRegistry $doctrine, Comment $comments, Article $article)
+    private function saveComment(PersistenceManagerRegistry $doctrine, Comment $comments)
     {
         $comments = $this->completeCommentBeforeSave($comments);
 
         $em = $doctrine->getManager();
-        $em->persist($article); // Persister l'entité Article
         $em->persist($comments);
         $em->flush();
     }
